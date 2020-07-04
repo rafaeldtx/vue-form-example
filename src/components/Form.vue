77dbd0e9ad4eaf5fc6c6f2da412c11759ed4d666
rafaeldtx <rafael991_@hotmail.com>
2020-07-04 20:32:33 -0300

    (Form/InputGroup) Added to App.vue

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,38 @@
 <template>
-  <span> Form.vue </span>
+    <div class="form card-grid">
+        <h1 class="form-title" v-if="title">{{ title }}</h1>
+        <hr v-if="title">
+
+        <input-group label='Name'>
+            <input type="text" name="name" id="name">
+        </input-group>
+
+        <input-group label='Email'>
+            <input type="email" name="email" id="email">
+        </input-group>
+
+        <input-group label='Password'>
+            <input type="password" name="password" id="password">
+        </input-group>
+    </div>
 </template>
 
 <script>
-export default {
+import InputGroup from './InputGroup'
 
+export default {
+    components: { InputGroup },
+    props: {
+        title: {
+            type: String,
+            default: null
+        }
+    }
 }
 </script>
 
 <style>
-
+.form .title {
+    text-align: center;
+}
 </style>
---
+++ src/components/InputGroup.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="input-group">
+        <label>{{ label }}:</label>
+        <div class="input">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['label']
+}
+</script>
+
+<style scoped>
+    .input-group {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin: 5px 0px;
+        font-size: 1.4rem;
+    }
+
+    .input {
+        display: flex;
+        align-self: stretch;
+        margin: 10px 0px;
+    }
+
+    input, textarea, select {
+        flex: 1;
+        font-size: 1.4rem;
+        outline: none;
+        border: 1px solid #DDD;
+        border-radius: 5px;
+        padding: 5px 10px;
+    }
+
+
+    input:focus {
+        border: 1px solid #BBB;
+    }
+</style>
